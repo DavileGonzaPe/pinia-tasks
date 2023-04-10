3d39ee9e1e41e9cbea7670c7d2c065ac9abb7c47
David González <dp@phoenixconnection.es>
2023-04-10 14:07:42 +0200

    Init project and principal components.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+    
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm/>
+    </div>
+
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'" >All tasks</button>
+      <button @click="filter = 'favs'" >Fav tasks</button>
+    </nav>
+
+    <!-- task list -->    
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { useTaskStore } from './store/TaskStore'
+import TaskDetails from './components/TaskDetails.vue'
+import { ref } from 'vue'
+import TaskForm from './components/TaskForm.vue'
+  export default {
+    components: {
+    TaskDetails,
+    TaskForm
+},
+    setup() {
+      const taskStore = useTaskStore()
+
+      const filter =  ref('all')
+
+      return { 
+        filter,
+        taskStore,
+      }
+    }
+  }
+</script>
+
---
+++ src/store/TaskStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('taskStore', {
+    state: () => ({
+        tasks: [
+            {id: 1, title: "buy some milk", isFav: false},
+            {id: 2, title: "play Gloomhaven", isFav: true}
+        ],
+    }),
+
+    getters: {
+        favs() {
+            return this.tasks.filter(task => task.isFav)
+        },
+        favCount() {
+            return this.tasks.reduce((previous, current) => {
+                return current.isFav ? previous + 1 : previous
+            }, 0)
+        },
+        totalCount: (state) => {
+            return state.tasks.length
+        }
+    },
+
+    actions: {
+        addTask(task) {
+            this.tasks.push(task)
+        }
+    }
+})
