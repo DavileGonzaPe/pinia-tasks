7367d95c559dfa72a1d1e70555842f53c066ebf5
David González <dp@phoenixconnection.es>
2023-04-10 15:16:48 +0200

    Resetting state and storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    
+
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
@@ -9,31 +9,33 @@
 
     <!-- new task form -->
     <div class="new-task-form">
-      <TaskForm/>
+      <TaskForm />
     </div>
 
     <!-- filter -->
     <nav class="filter">
-      <button @click="filter = 'all'" >All tasks</button>
-      <button @click="filter = 'favs'" >Fav tasks</button>
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
     </nav>
 
     <!-- loading -->
-    <div class="loading" v-if="taskStore.isLoading">Loading tasks...</div>
+    <div class="loading" v-if="isLoading">Loading tasks...</div>
 
-    <!-- task list -->    
+    <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks">
-        <TaskDetails :task="task"/>
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks">
+        <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} favs left to do</p>
-      <div v-for="task in taskStore.favs">
-        <TaskDetails :task="task"/>
+      <p>You have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs">
+        <TaskDetails :task="task" />
       </div>
     </div>
+
+    <button @click="taskStore.$reset">Reset Store</button>
   </main>
 </template>
 
@@ -42,24 +44,32 @@ import { useTaskStore } from './store/TaskStore'
 import TaskDetails from './components/TaskDetails.vue'
 import { ref } from 'vue'
 import TaskForm from './components/TaskForm.vue'
-  export default {
-    components: {
+import { storeToRefs } from 'pinia'
+export default {
+  components: {
     TaskDetails,
     TaskForm
-},
-    setup() {
-      const taskStore = useTaskStore()
+  },
+  setup() {
+    const taskStore = useTaskStore()
 
-      //fetch tasks
-      taskStore.getTasks()
+    const { tasks, isLoading, favs, totalCount, favCount } = storeToRefs(taskStore)
 
-      const filter =  ref('all')
+    //fetch tasks
+    taskStore.getTasks()
 
-      return { 
-        filter,
-        taskStore,
-      }
+    const filter = ref('all')
+
+    return {
+      favCount,
+      favs, 
+      filter,
+      isLoading, 
+      tasks, 
+      taskStore,
+      totalCount, 
     }
   }
+}
 </script>
 
